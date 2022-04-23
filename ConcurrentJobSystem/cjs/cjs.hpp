96308107e591664b4adb106987a667b2121afbca
Domara Shlimon <domara2000@gmail.com>
2022-04-22 20:48:23 -0400

    reworked library to be contained within one header file

--- ConcurrentJobSystem/cjs/cjs.hpp
+++ ConcurrentJobSystem/cjs/cjs.hpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2021 Domara Shlimon
+Copyright (c) 2022 Domara Shlimon
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,14 +23,186 @@ SOFTWARE.
 */
 #ifndef CJS_CJS_HPP
 #define CJS_CJS_HPP
-#include "ijob.hpp"
-#include "context.hpp"
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <list>
+#include <vector>
+#include <memory>
 
 namespace cjs {
 
-	using handle = basic_handle<ijob>;
+	template<typename T>
+	using shared = std::shared_ptr<T>;
 
-	using context = basic_context<handle>;
+	// basic interface for jobs
+	struct ijob {
+		virtual ~ijob() = 0 { }
+		virtual void execute() = 0;
+	};
+
+	// completion status of job
+	struct job_status final {
+		enum type : uint8_t {
+			incomplete,
+			complete
+		};
+
+		job_status(type val = incomplete) : m_val(val) { }
+
+		job_status& operator=(type val) {
+			m_val = val;
+			return *this;
+		}
+
+		bool operator==(job_status other) const {
+			return m_val == other.m_val;
+		}
+
+		bool operator!=(job_status other) const {
+			return m_val != other.m_val;
+		}
+
+	private:
+		std::atomic<type> m_val;
+	};
+
+	// awaits for the job_status value to become job_status::complete
+	void await_complete(const job_status* status);
+
+	// awaits for the job_status value to become job_status::complete
+	void await_complete(const job_status& status);
+
+	// awaits for all job_status values to become job_status::complete
+	template<typename RangeType>
+	void await_complete(RangeType begin, RangeType end);
+
+	// handles creating worker threads and distributing jobs to different threads
+	template<typename ThreadTy>
+	class basic_context final {
+	public:
+		basic_context(const basic_context&) = delete;
+		basic_context& operator=(const basic_context&) = delete;
+		basic_context(basic_context&&) = delete;
+		basic_context& operator=(basic_context&&) = delete;
+
+		// starts the worker threads
+		// defaults to the maximum available threads on the system
+		basic_context(size_t workerCount = 0);
+
+		// closes the worker threads
+		~basic_context();
+
+
+		// submits a function to be worked on
+		// optional completion status 
+		void submit(std::function<void()> func, job_status* status = nullptr);
+
+		// submits a job to be worked on
+		// optional completion status 
+		void submit(shared<ijob> job, job_status* status = nullptr);
+
+
+	private:
+		struct job_node final {
+			std::function<void()> func;
+			shared<ijob> job;
+			job_status* status = nullptr;
+		};
+		std::atomic_bool m_exit;
+		std::mutex m_lock;
+		std::list<job_node> m_jobs;
+		std::vector<ThreadTy> m_threads;
+
+		static void _worker_thread(const size_t index, basic_context* c);
+	};
+
+	using context = basic_context<std::thread>;
+
+	// implementations
+
+	inline void await_complete(const job_status* status) {
+		while (*status != job_status::complete) { }
+	}
+	
+	inline void await_complete(const job_status& status) {
+		await_complete(&status);
+	}
+
+	template<typename RangeType>
+	inline void await_complete(RangeType begin, RangeType end) {
+		for (auto it = begin; it != end; it++) {
+			await_complete(&*it);
+		}
+	}
+
+	template<typename ThreadTy>
+	inline basic_context<ThreadTy>::basic_context(size_t workerCount) : m_exit(false) {
+		if (workerCount == 0)
+			workerCount = std::thread::hardware_concurrency() - 1;
+		m_threads.resize(workerCount);
+		for (size_t i = 0; i < m_threads.size(); i++) {
+			m_threads[i] = std::thread(&basic_context<ThreadTy>::_worker_thread, i, this);
+		}
+	}
+
+	template<typename ThreadTy>
+	inline basic_context<ThreadTy>::~basic_context() {
+		m_exit = true; // tell the worker threads to quit
+		for (size_t i = 0; i < m_threads.size(); i++) {
+			m_threads[i].join();
+		}
+	}
+
+	template<typename ThreadTy>
+	inline void basic_context<ThreadTy>::submit(std::function<void()> func, job_status* status) {
+		job_node n;
+		n.func = func;
+		n.status = status;
+		*status = job_status::incomplete;
+		m_jobs.push_back(n);
+	}
+
+	template<typename ThreadTy>
+	inline void basic_context<ThreadTy>::submit(shared<ijob> job, job_status* status) {
+		job_node n;
+		n.job = job;
+		n.status = status;
+		*status = job_status::incomplete;
+		m_jobs.push_back(n);
+	}
+
+	template<typename ThreadTy>
+	inline void basic_context<ThreadTy>::_worker_thread(const size_t index, basic_context* c) {
+		job_node n;
+		while (!c->m_exit) {
+			// reset
+			n.func = nullptr;
+			n.job.reset();
+			n.status = nullptr;
+
+			// wait for job
+			while ((!n.func && !n.job) && !c->m_exit) {
+				std::lock_guard<std::mutex> _0(c->m_lock);
+
+				// check if there are any jobs
+				if (c->m_jobs.size() > 0) {
+					n = c->m_jobs.front();
+					c->m_jobs.pop_front();
+				}
+			}
+
+			// if there is a job, run it
+			if (!n.func != !n.job) {
+				// run job
+				if (n.func) n.func();
+				if (n.job) n.job->execute();
+				// set status
+				if (n.status) *(n.status) = job_status::complete;
+			}
+		}
+	}
 
 }
 
--- TestingGround/Main.cpp
+++ TestingGround/Main.cpp
@@ -4,6 +4,7 @@
 #define REPEAT 70000000
 #include <cjs\cjs.hpp>
 
+
 struct Somejob final : public cjs::ijob {
 	double a = 0.0;
 	void execute() override {
@@ -14,47 +15,62 @@ struct Somejob final : public cjs::ijob {
 };
 
 void ThreadingTest() {
-	std::array<Somejob, 10> jobs;
-	for (size_t i = 0; i < jobs.size(); i++) jobs[i].a = (double)i;
-	
+	struct jobpair {
+		cjs::job_status status;
+		cjs::shared<Somejob> job;
+	};
+
+	constexpr size_t count = 10;
+	std::array<jobpair, count> jobs;
+	for (size_t i = 0; i < jobs.size(); i++) {
+		jobs[i].job.reset(new Somejob());
+		jobs[i].job->a = (double)i;
+	}
+
 	// queue and handle
-	cjs::context context(10);
-	cjs::handle handle;
-	
+	cjs::context* context = new cjs::context();
+
 	// start timer
 	std::chrono::steady_clock::time_point _start(std::chrono::steady_clock::now());
-	
+
 	// add jobs to handle and then submit
-	for (auto& job : jobs) handle += &job;
-	context.submit(&handle);
-	
+	for (size_t i = 0; i < count; ++i) context->submit(jobs[i].job, &(jobs[i].status));
+
 	// await
-	handle.await_complete();
-	handle.clear();
-	
+	for (auto& job : jobs) cjs::await_complete(&(job.status));
+
 	// end timer and print
 	std::cout << "Multithreaded test: ";
 	std::chrono::steady_clock::time_point _end(std::chrono::steady_clock::now());
 	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(_end - _start).count() << std::endl;
-	
+
 }
 
 void NormalTest() {
 	std::array<Somejob, 10> jobs;
 	for (size_t i = 0; i < jobs.size(); i++) jobs[i].a = (double)i;
-	
+
 	// start timer
 	std::chrono::steady_clock::time_point _start(std::chrono::steady_clock::now());
-	
+
 	// run
 	for (auto& job : jobs) job.execute();
-	
+
 	// end timer and print
 	std::chrono::steady_clock::time_point _end(std::chrono::steady_clock::now());
 	std::cout << "Single thread test: ";
 	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(_end - _start).count() << std::endl;
-	
+
 }
+
+struct JobTest : public cjs::ijob {
+	~JobTest() {
+		printf("Deleted!\n");
+	}
+	void execute() override {
+		printf("Test!\n");
+	}
+};
 
 int main(int argc, char** argv) {
 
